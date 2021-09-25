579518c225ac5952495a030c2b5a00c5dc5b4093
Anik <baust.cse.160201078@gmail.com>
2021-09-26 00:15:50 +0600

    Hheader

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header-style">
+            <h1>Let's Rock !!!</h1>
+            <p>Chose bands for your concert</p>
+            <h3>Total Budget : 50k</h3>
+
+        </div>
+    );
+};
+
+export default Header;
